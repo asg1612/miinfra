7cf1228243db799a669b892af0f5a1bbd56557a5
Andres <asg1612@gmail.com>
2021-08-02 10:14:56 +0200

    primer commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="miinfra-asg1612",
+    version="0.0.1",
+    author="Andrés Sánchez García",
+    author_email="asg1612@gmail.com",
+    description="cli para controlar mi infraestructura",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asg1612/miinfra",
+    project_urls={
+        "Bug Tracker": "https://github.com/asg1612/miinfra/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved ::  GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ src/miproxmox/connect.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+:copyright: (c) 2021 by Andrés Sánchez García.
+:license: Apache2, see LICENSE for more details.
+"""
+
+def add_one(number):
+    return number + 1

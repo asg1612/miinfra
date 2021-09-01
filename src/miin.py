d122f1e9f5284b395b4a95c3c0660d34d7a29826
Andres <asg1612@gmail.com>
2021-09-01 13:54:54 +0200

    sincronizacion

--- src/miin.py
+++ src/miin.py
@@ -5,3 +5,16 @@
 :copyright: (c) 2021 by Andrés Sánchez García.
 :license: Apache2, see LICENSE for more details.
 """
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+if __name__ == '__main__':
+    hello()
